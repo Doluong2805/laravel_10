bcb1bd0fb2bd656e2e7d8c66852d500a05f6a7a2
= <quoclongdng@gmail.com>
2023-03-04 19:46:05 +0700

    fix

---
+++ resources/views/client/master.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en" class="no-js">
+
+<head>
+    @include('client.css')
+</head>
+
+<body>
+    <div class="body-wrapper">
+        @include('client.header')
+
+        @include('client.menu')
+
+        @yield('noi_dung')
+
+        @include('client.footer')
+
+        @include('client.js')
+        @yield('js')
+    </div>
+</body>
+
+</html>
