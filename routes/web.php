b6407ec31b2690bf04c363c0a990aca7035c8363
= <quoclongdng@gmail.com>
2023-03-23 20:49:43 +0700

    middle

--- routes/web.php
+++ routes/web.php
@@ -63,7 +63,7 @@ Route::post('/customer/register', [KhachHangController::class, 'actionRegister']
 Route::get('/customer/login', [KhachHangController::class, 'viewLogin']);
 Route::post('/customer/login', [KhachHangController::class, 'actionLogin']);
 
-Route::group(['prefix' => '/admin'], function() { //, 'middleware' => 'authadmin'
+Route::group(['prefix' => '/admin', 'middleware' => 'authadmin'], function() { //, 'middleware' => 'authadmin'
     // Route của Chuyên Mục
     Route::group(['prefix' => '/chuyen-muc'], function() {
         Route::get('/index', [ChuyenMucController::class, 'index']);
